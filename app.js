f6623a6d3b86d6f5dc01066335f63d0e7dbd77e5
Eberte Sampaio <deveberte@gmail.com>
2025-07-30 09:41:51 -0300

    feat: Implementar sorteio e resultado do sorteio

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let friendsList     = [];
+const tagFriendsList  = document.getElementById("listaAmigos");
+const tagWinner       = document.getElementById("resultado")
+const responseMessage = document.getElementById("resposta");
+const inputFriendName = document.getElementById("amigo");
+
+
+function adicionarAmigo(){
+    let friendName = inputFriendName.value;
+
+    if(friendName == null || friendName == ""){
+        showMessage("Por gentileza, insira o nome do usuário.", "red");
+        return;
+    }
+
+    friendsList.push(friendName);
+    tagFriendsList.innerHTML = friendsList;
+    
+    showMessage(friendName+" foi inserido com sucesso!", "green");
+}
+
+function sortearAmigo(){
+    let sizeList = friendsList.length;
+
+    if(sizeList <= 2){
+        showMessage("Você deve inserir ao menos duas pessoas na lista.");
+        return;
+    }
+
+    let index = Math.floor(Math.random() * sizeList);
+    let result = friendsList[index];
+    tagWinner.innerHTML = result;
+    friendsList = [];
+
+    showMessage("Para jogar novamente, basta inserir os nomes novamente!", "green");
+}
+
+function showMessage(message, color){
+    responseMessage.style.color = color;
+    responseMessage.innerHTML   = message;
+    inputFriendName.value       = null;
+}
